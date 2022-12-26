1bc7f0451e4b09d2d53e6d0e137a327103829f40
FATZNG <phpsarc@gmail.com>
2022-12-26 13:36:51 +0800

    abstract Factory
    Signed-off-by: FATZNG <phpsarc@gmail.com>

--- creational/AbstractFactory/LinuxCvsWriter.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);
-namespace Designpatten\creational\abstractFactory;
-class LinuxCvsWriter implements CvsWriter{
-    public function writer(array $data):string{
-        return join(',',$data).PHP_EOL;
-    }
-}
--- creational/AbstractFactory/LinuxJsonWriter.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types = 1);
-namespace Designpatten\creational\abstractFactory;
-class LinuxJsonWriter implements JsonWriter
-{
-    public function writer(array $data):string{
-        return json_encode($data);
-    }
-}
